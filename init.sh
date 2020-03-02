6a36cf5849423e098f762a49a37c54cd9f80d2a6
rokiyama <57606749+rokiyama@users.noreply.github.com>
2020-03-02 19:27:26 +0900

    remove brewfile

--- init.sh
+++ init.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -ux
 
+# prerequisites: xcode cli tools and homebrew are installed
+if !(type brew > /dev/null 2>&1); then
+  echo 'brew does not exist' 1>&2
+  exit 1
+fi
+
 DOTFILES_DIR=~/.dotfiles
 
 [[ -d $DOTFILES_DIR ]] || git clone https://github.com/rokiyama/dotfiles.git $DOTFILES_DIR
@@ -12,14 +18,57 @@ git submodule update
 
 [ $OSTYPE != "msys" ] && source ./linktohome.sh
 
-# xcode
-xcode-select --install || echo 'xcode seems to be already installed'
+# brew
+brew install awscli
+brew install bat
+brew install coreutils
+brew install fish
+brew install fzf
+brew install gdbm
+brew install gettext
+brew install ghq
+brew install git
+brew install go
+brew install hadolint
+brew install jq
+brew install libevent
+brew install libtermkey
+brew install libuv
+brew install libvterm
+brew install luajit
+brew install mas
+brew install msgpack
+brew install neovim
+brew install nvm
+brew install oniguruma
+brew install openssl
+brew install pcre2
+brew install pkg-config
+brew install python
+brew install rbenv
+brew install readline
+brew install reattach-to-user-namespace
+brew install ripgrep
+brew install ruby-build
+brew install sqlite
+brew install tmux
+brew install trash
+brew install unar
+brew install unibilium
+brew install wakeonlan
+brew install xz
 
-# homebrew & brew-file
-curl -sSL https://raw.github.com/rcmdnk/homebrew-file/install/install.sh | sh
-export HOMEBREW_BREWFILE=$HOME/.dotfiles/Brewfile
-export HOMEBREW_BREWFILE_ON_REQUEST=1
-brew-file install
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
+brew cask install alacritty
+brew cask install chrome
+brew cask install discord
+brew cask install docker
+brew cask install font-fira-mono-for-powerline
+brew cask install google-backup-and-sync
+brew cask install karabiner-elements
+brew cask install mattermost
+brew cask install visual-studio-code
 
 # python
 type pip3 > /dev/null 2>&1 && pip3 install pip-review neovim pynvim powerline-status powerline-gitstatus Send2Trash
