63001b484cd532b948e7ffa35c7ccfa3ee30874d
rokiyama <57606749+rokiyama@users.noreply.github.com>
2021-04-19 10:56:57 +0900

    fix for windows

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -uex
 
+[ $OSTYPE = "linux-gnu" ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 if !(type brew > /dev/null 2>&1); then
   curl -fsSLo- https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
 fi
-[ $OSTYPE = "linux-gnu" ] && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
 DOTFILES_DIR=~/.dotfiles
 
@@ -44,7 +44,6 @@ brew install \
   starship \
   svn \
   tmux \
-  unar \
   unibilium \
   xz
 
@@ -69,6 +68,7 @@ if [[ $OSTYPE =~ ^darwin ]]; then
     readline \
     reattach-to-user-namespace \
     trash \
+    unar \
     wakeonlan \
     watch
 
@@ -90,14 +90,19 @@ if [[ $OSTYPE =~ ^darwin ]]; then
     visual-studio-code \
     zoom
 
+  # fonts
+  brew tap homebrew/cask-fonts
+  brew install --cask \
+    font-fira-mono-nerd-font
+
   # mas
   mas install 497799835  # Xcode
   mas install 1352778147 # Bitwarden
 fi
 
 # fish
-if type fish 2>/dev/null; then
-  readonly FISH_PATH=$(which fish)
+readonly FISH_PATH=$(brew --prefix)/bin/fish
+if [ -f $FISH_PATH ]; then
   grep -x $FISH_PATH /etc/shells || sudo sh -c "echo $FISH_PATH >> /etc/shells"
   if [ $SHELL != $(which fish) ]; then
     chsh -s $FISH_PATH
@@ -106,10 +111,5 @@ fi
 
 # python
 type pip3 > /dev/null 2>&1 && pip3 install --user --upgrade pip-review pynvim powerline-status powerline-gitstatus Send2Trash
-
-# fonts
-brew tap homebrew/cask-fonts
-brew install --cask \
-  font-fira-mono-nerd-font
 
 [ $OSTYPE != "msys" ] && source ./linktohome.sh
