0d5e297ae1d9fffd7bdeec4042aba5bac3542de5
rokiyama <57606749+rokiyama@users.noreply.github.com>
2021-04-19 11:04:09 +0900

    add for linux

---
+++ fish/universal/linuxbrew.fish
@@ -0,0 +1,5 @@
+set -Ux HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew";
+set -Ux HOMEBREW_CELLAR "/home/linuxbrew/.linuxbrew/Cellar";
+set -Ux HOMEBREW_REPOSITORY "/home/linuxbrew/.linuxbrew/Homebrew";
+fish_add_path /home/linuxbrew/.linuxbrew/bin
+fish_add_path /home/linuxbrew/.linuxbrew/sbin
