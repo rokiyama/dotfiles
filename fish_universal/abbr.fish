589f219e4d03ac0858583621e5d0aeb33dec4666
rokiyama <57606749+rokiyama@users.noreply.github.com>
2022-11-28 18:25:57 +0900

    fish: Add abbr

--- fish_universal/abbr.fish
+++ fish_universal/abbr.fish
@@ -41,6 +41,7 @@ abbr gps  git push
 abbr gr   git remote -v
 abbr gbs  fzf_git_switch_branch
 abbr gbsa fzf_git_switch_branch --all
+abbr gst  'git switch'
 abbr gbd  fzf_git_delete_branch
 abbr gcw  fzf_git_cd_worktree
 abbr gw   git worktree
